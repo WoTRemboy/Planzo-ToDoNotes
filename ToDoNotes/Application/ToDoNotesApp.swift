1fdfa4c50b3b639340e45759f6cbfd576efc7297
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-01-01 01:39:58 +0200

    init

---
+++ ToDoNotes/Application/ToDoNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoNotesApp.swift
+//  ToDoNotes
+//
+//  Created by Roman Tverdokhleb on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
