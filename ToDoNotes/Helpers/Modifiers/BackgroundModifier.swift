6dba7199509a5bc7725875433cf46717dee10dde
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2026-02-27 19:22:07 +0200

    feat: added liquid glass ui today page

---
+++ ToDoNotes/Helpers/Modifiers/BackgroundModifier.swift
@@ -0,0 +1,26 @@
+//
+//  BackgroundModifier.swift
+//  ToDoNotes
+//
+//  Created by Roman Tverdokhleb on 2/27/26.
+//
+
+import SwiftUI
+
+private struct BackgroundModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        if #available(iOS 26.0, *) {
+            content.background(Color.BackColors.backDefaultGlass)
+        } else {
+            content
+                .shadow(color: Color.ShadowColors.taskSection, radius: 10, x: 2, y: 2)
+                .background(Color.BackColors.backDefault)
+        }
+    }
+}
+
+extension View {
+    func defaultBackgroundStyle() -> some View {
+        modifier(BackgroundModifier())
+    }
+}
